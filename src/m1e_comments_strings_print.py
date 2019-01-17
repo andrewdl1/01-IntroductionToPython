dadc8fccbbb95ff05b4a3cd0dc4ea218fd8b9e7f
andrewdl <andrewdl@rose-hulman.edu>
2019-01-17 09:39:40 -0500

    I finished the first to do.

--- src/m1e_comments_strings_print.py
+++ src/m1e_comments_strings_print.py
@@ -22,7 +22,7 @@
 ###############################################################################
 
 
-print('Hello, World')
+print('Hello, Dalton')
 print('hi there')
 print('one', 'two', 'buckle my shoe')
 
